2eac22784910d687709678507311674b35b1f232
Nico Read <72672185+nicoreadm@users.noreply.github.com>
2023-11-22 20:33:32 -0300

    Update calculadora.py
    Al momento de convertir los str de los valores de los tipos de USD a float, me daba un error para convertirlo dado que el str en si contenía dos '.' .

--- CDTR - Web Scraping- Tkinter/calculadora.py
+++ CDTR - Web Scraping- Tkinter/calculadora.py
@@ -1,23 +1,50 @@
+def convertir_float(valor):
+  valor= '1.080.00'
+  valor = valor.replace('.','')
+  valor= valor[::-1]
+  i=0
+  valor2=''
+  for caracter in valor:
+    valor2=valor2+caracter
+    i+=1
+    if i ==2:
+      valor2=valor2+'.'
+
+  valor2= valor2[::-1]
+  return valor2
+
 def fcalculadora(blue,bna,mep,ccl,usdt,ingresar,vector):
 
   #CALCULADORA
   pes = ingresar
+
+  #convertir en float
   
+  #blue
+  fblue=convertir_float(blue[1])
+  #bna
+  fbna=convertir_float(bna[1])
+  #mep
+  fmep=convertir_float(mep[1])
+  #ccl
+  fccl=convertir_float(ccl[1])
+  #usdt
+  fusdt=convertir_float(usdt[1])
+
   #dolar blue
-  peblue = round(float(pes) / float(blue[1]),2)
+  peblue = round(float(pes) / float(fblue),2)
 
   #dolar bna
-  pbna = round(float(pes) / float(bna[1]),2)
-  
+  pbna = round(float(pes) / float(fbna),2)
 
   #dolar mep
-  pmep = round(float(pes) / float(mep[1]),2)
+  pmep = round(float(pes) / float(fmep),2)
 
   #dolar ccl
-  pccl = round(float(pes) / float(ccl[1]),2)
+  pccl = round(float(pes) / float(fccl),2)
   
   #usdt
-  pusdt= round(float(pes) / float(usdt[1]),2)
+  pusdt= round(float(pes) / float(fusdt),2)
   vector[0] = str(peblue)
   vector[1] = str(pbna)
   vector[2] = str(pmep)  
